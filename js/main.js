3625bf0908f5cbd9f112de143e65891107d5c7ee
Александр <al.silantyev@mail.ru>
2022-06-10 21:11:14 +0300

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,28 +1,21 @@
 // Пример кода взят с https://learn.javascript.ru/task/random-int-min-max
-const getRandomNumb = function(minNumb, maxNumb) {
-  let result;
-  result = Math.round(minNumb - 0.5 + Math.random() * (maxNumb - minNumb + 1));
-  if (minNumb > maxNumb) {
-    result = 'Некорректный диапазон чисел. Минимальное число должно быть меньше максимального!';
+function getRandomNumber (min, max) {
+  if (min >= 0 && max > min) {
+    return Math.round(min - 0.5 + Math.random() * (max - min + 1));
   }
-  if (minNumb < 0) {
-    result = 'Некорректный диапазон чисел. Минимальное число должно быть больше 0!';
-  }
-  return result;
-};
+}
 
-getRandomNumb(0, 10);
+getRandomNumber(-5, 10);
 
 
 // Проверка на максимальное количество вводимых символов
 
-const COMMENT_FIELD = document.querySelector('.social__footer-text');
-const COMMENT_BUTTON = document.querySelector('.social__footer-btn');
-
-COMMENT_FIELD.oninput = function (commentArea, commentLength) {
-  if (commentArea.value.length > commentLength ) {
-    COMMENT_BUTTON.disabled = true;
+function checkoutTextLength (textArea, maxLength) {
+  if (textArea.value.length <= maxLength ) {
+    return true;
   } else {
-    COMMENT_BUTTON.disabled = false;
+    return false;
   }
-};
+}
+
+checkoutTextLength();
