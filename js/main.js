fbc479a092236e642f106640bc24263d67ce4ad0
Александр <al.silantyev@mail.ru>
2022-06-14 18:32:06 +0300

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -2,20 +2,18 @@
 function getRandomNumber (min, max) {
   if (min >= 0 && max > min) {
     return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+  } else {
+    throw new Error('Некорректный диапазон чисел.');
   }
 }
 
-getRandomNumber(-5, 10);
+getRandomNumber(-5, 16);
 
 
 // Проверка на максимальное количество вводимых символов
 
-function checkoutTextLength (textArea, maxLength) {
-  if (textArea.value.length <= maxLength ) {
-    return true;
-  } else {
-    return false;
-  }
+function checkoutTextLength (text, maxLength) {
+  return text.length <= maxLength;
 }
 
 checkoutTextLength();
