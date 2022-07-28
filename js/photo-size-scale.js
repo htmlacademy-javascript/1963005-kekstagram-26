cb523a66815272c897a277512d802bb8658b7394
Александр <al.silantyev@mail.ru>
2022-07-28 15:46:01 +0300

    9.9. Помощь друга

---
+++ js/photo-size-scale.js
@@ -0,0 +1,36 @@
+const SIZE_SCALE_SETTINGS = {
+  min: 25,
+  max: 100,
+  step: 25,
+};
+
+const uploadedPhoto = document.querySelector('.img-upload__preview img');
+const photoSizeSmallerButton = document.querySelector('.scale__control--smaller');
+const photoSizeBiggerButton = document.querySelector('.scale__control--bigger');
+const photoSizeScaleInput = document.querySelector('.scale__control--value');
+
+const resetPhotoSizeValue = () => {
+  photoSizeScaleInput.value = `${SIZE_SCALE_SETTINGS.max}`;
+};
+
+const changeScaleValue = (increase) => {
+  let photoSizeScaleValue = parseInt(photoSizeScaleInput.value, 10);
+  if (increase) {
+    if (photoSizeScaleValue < SIZE_SCALE_SETTINGS.max) {
+      photoSizeScaleValue += SIZE_SCALE_SETTINGS.step;
+    }
+  } else if (photoSizeScaleValue > SIZE_SCALE_SETTINGS.min) {
+    photoSizeScaleValue -= SIZE_SCALE_SETTINGS.step;
+  }
+
+  photoSizeScaleInput.value = `${photoSizeScaleValue}`;
+  uploadedPhoto.style.transform = `scale(${photoSizeScaleValue / 100})`;
+};
+
+const onIncreaseScaleButtonClick = () => changeScaleValue(true);
+const onDecreaseScaleButtonClick = () => changeScaleValue(false);
+photoSizeBiggerButton.addEventListener('click', onIncreaseScaleButtonClick);
+photoSizeSmallerButton.addEventListener('click', onDecreaseScaleButtonClick);
+
+export { resetPhotoSizeValue };
+
--- js/upload-photo-form.js
+++ js/upload-photo-form.js
@@ -1,4 +1,10 @@
-import { isEscKeydown } from './util.js';
+import { checkoutTextLength, isEscKeydown } from './util.js';
+import { resetPhotoSizeValue } from './photo-size-scale.js';
+import { resetPhotoEffects } from './add-photo-effects.js';
+
+const PHOTO_DESCRIPTION_MAX_LENGTH = 140;
+const HASHTAG_REGULAR_EXPRESSION = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+const MAX_TAGS = 5;
 
 const pageBody = document.body;
 
@@ -14,6 +20,9 @@ const photoDescriptionField = document.querySelector('.text__description');
 const openUploadForm = () => {
   pageBody.classList.add('modal-open');
   uploadOverlay.classList.remove('hidden');
+
+  resetPhotoSizeValue();
+  resetPhotoEffects();
 
   const onUploadFormEscKeydown = (evt) => {
     if (isEscKeydown(evt)) {
@@ -54,5 +63,70 @@ const openPhotoUploadForm = () => {
   uploadFormFileInput.addEventListener('change', openUploadForm);
 };
 
+//Валидация
+
+const validatePhotoDescriptionLength = (string) => checkoutTextLength(string, PHOTO_DESCRIPTION_MAX_LENGTH);
+
+const validateHashtagsNames = (data) => {
+  if (data.length > 0) {
+    const hashtagsArray = data.split(' ');
+    return(hashtagsArray.every((hashtag) => HASHTAG_REGULAR_EXPRESSION.test(hashtag)));
+  }
+  return true;
+};
+
+const validateHashtagsAmount = (data) => {
+  const hashtagsArray = data.split(' ');
+  return(hashtagsArray.length <= MAX_TAGS);
+};
+
+const validateHashtagsDublicates = (data) => {
+  const hashtagsArray = data.split(' ');
+  const uniqueHashtagsArray = [...new Set(hashtagsArray.map((hashtag) => hashtag.toLowerCase()))];
+  return (hashtagsArray.length === uniqueHashtagsArray.length);
+};
+
+const pristine = new Pristine(uploadForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorClass: 'img-upload__item--invalid',
+  successClass: 'img-upload__item--valid',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div'
+});
+
+//Проверка максимальной длины строки описания фотографии
+pristine.addValidator(
+  photoDescriptionField,
+  validatePhotoDescriptionLength,
+  'Длина комментария не может составлять больше 140 символов'
+);
+
+//Проверка именования Тега на "маску" разрешенного именования
+pristine.addValidator(
+  hashtagsField,
+  validateHashtagsNames,
+  'Хэш-тег должен начинается с символа #, состоять из букв и чисел, не содержать пробелы, спецсимволы, содержать от 2 до 20 символов'
+);
+
+//Проверка на максимольное допустимое количество Тегов
+pristine.addValidator(
+  hashtagsField,
+  validateHashtagsAmount,
+  `Максимальное количство хэш-тегов не должно превышать ${MAX_TAGS}`
+);
+
+//Проверка Тегов на отсутсвие их повторений
+pristine.addValidator(
+  hashtagsField,
+  validateHashtagsDublicates,
+  'Хэш-теги не должны повторяться'
+);
+
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  if (pristine.validate()) {
+    uploadForm.submit();
+  }
+});
 
 export { openPhotoUploadForm };
